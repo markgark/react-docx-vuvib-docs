6e466e232970862b286971effc83f100b4c12bb9
markgark <mvgarcia99@hotmail.com>
2022-06-29 16:22:43 -0500

    1 file modified

--- cv-generator.ts
+++ cv-generator.ts
@@ -5,6 +5,8 @@ import {
   Packer,
   Paragraph,
   ImageRun,
+  Header,
+  Footer,
   Table,
   TableCell,
   TableRow,
@@ -38,6 +40,20 @@ export class DocumentCreator {
     const document = new Document({
       sections: [
         {
+          headers: {
+            default: new Header({
+              children: [
+                this.createLogoHeaderSenescyt(),
+              ],
+            }),
+          },
+          footers: {
+            default: new Footer({
+              children: [
+                 this.createLogoFooterSenescyt(),
+              ],
+            }),
+          },
           children: [
             new Paragraph({
               text: "Dolan Miu",
@@ -126,11 +142,7 @@ export class DocumentCreator {
   }
 
 
-  public createLogoHeading(
-    entidad: string,
-    escudo: Blob,
-    senescyt: Blob
-    ): Table {
+  public createLogoHeaderSenescyt(): Table {
     return new Table({
       rows: [
         new TableRow({
@@ -164,6 +176,89 @@ export class DocumentCreator {
                       },
                     }),
                   ],
+                }),
+              ],
+              verticalAlign: VerticalAlign.CENTER,
+            }),
+          ],
+        }),
+      ],
+      width: {
+        size: 100,
+        type: WidthType.PERCENTAGE,
+      },
+      borders: {
+        top: {
+          style: BorderStyle.NONE,
+        },
+        bottom: {
+          style: BorderStyle.NONE,
+        },
+        left: {
+          style: BorderStyle.NONE,
+        },
+        right: {
+          style: BorderStyle.NONE,
+        },
+        insideVertical: {
+          style: BorderStyle.NONE,
+        }
+      },
+    });
+  }
+
+
+  public createLogoFooterSenescyt(): Table {
+    return new Table({
+      rows: [
+        new TableRow({
+          children: [
+            new TableCell({
+              children: [
+                new Paragraph({
+                  children:[
+                    new TextRun({
+                      text: "Dirección: Edificio Matriz: Alpallana E7-183 entre Av. Diego de Almagro y Whymper",
+                      font: "Arial",
+                      size: 12,
+                    }),
+                  ],
+                }),
+                new Paragraph({
+                  children:[
+                    new TextRun({
+                      text: "Código Postal: 170518 / Quito - Ecuador",
+                      font: "Arial",
+                      size: 12
+                    }),
+                  ]
+                }),
+                new Paragraph({
+                  children:[
+                    new TextRun({
+                      text: "Teléfono: 593-2 3934-300",
+                      font: "Arial",
+                      size: 12
+                    }),
+                  ]
+                }),
+              ],
+              verticalAlign: VerticalAlign.CENTER,
+            }),
+            new TableCell({
+              children: [
+                new Paragraph({
+                  alignment: AlignmentType.END,
+                  children: [
+                    new ImageRun({
+                      data: gobiernodetodos,
+                      transformation: {
+                        width: 190,
+                        height: 66,
+                      },
+                    }),
+                  ],
+                  // alineación de la figura a la derecha
                 }),
               ],
               verticalAlign: VerticalAlign.CENTER,
