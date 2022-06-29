f50bdb5fe45dde3088946f1f74ad560541c53f61
markgark <mvgarcia99@hotmail.com>
2022-06-29 15:52:47 -0500

    1 file modified, 1 added

--- cv-generator.ts
+++ cv-generator.ts
@@ -4,6 +4,14 @@ import {
   HeadingLevel,
   Packer,
   Paragraph,
+  ImageRun,
+  Table,
+  TableCell,
+  TableRow,
+  TableBorders,
+  VerticalAlign,
+  WidthType,
+  BorderStyle,
   TabStopPosition,
   TabStopType,
   TextRun
@@ -11,6 +19,18 @@ import {
 const PHONE_NUMBER = "07534563401";
 const PROFILE_URL = "https://www.linkedin.com/in/dolan1";
 const EMAIL = "docx@docx.com";
+
+const escudo = fetch(
+  'https://cdn.jsdelivr.net/gh/markgark/react-ts-vuvib@main/imagenes/republica-ecuador-escudo.png'
+  ).then((r) => r.blob())
+
+const senescyt = fetch(
+  'https://cdn.jsdelivr.net/gh/markgark/react-ts-vuvib@main/imagenes/logo-senescyt.jpeg'
+).then((r) => r.blob());
+
+const gobiernodetodos = fetch(
+    'https://cdn.jsdelivr.net/gh/markgark/react-ts-vuvib@main/imagenes/gobierno-de-todos.png'
+).then((r) => r.blob());
 
 export class DocumentCreator {
   // tslint:disable-next-line: typedef
@@ -103,6 +123,76 @@ export class DocumentCreator {
     });
 
     return document;
+  }
+
+
+  public createLogoHeading(
+    entidad: string,
+    escudo: Blob,
+    senescyt: Blob
+    ): Table {
+    return new Table({
+      rows: [
+        new TableRow({
+          children: [
+            new TableCell({
+              children: [
+                new Paragraph({
+                  children: [
+                    new ImageRun({
+                      data: escudo,
+                      transformation: {
+                        width: 175,
+                        height: 94,
+                      },
+                    }),
+                  ],
+                }),
+              ],
+              verticalAlign: VerticalAlign.CENTER,
+            }),
+            new TableCell({
+              children: [
+                new Paragraph({
+                  alignment: AlignmentType.END,
+                  children: [
+                    new ImageRun({
+                      data: senescyt,
+                      transformation: {
+                        width: 300,
+                        height: 32,
+                      },
+                    }),
+                  ],
+                }),
+              ],
+              verticalAlign: VerticalAlign.CENTER,
+            }),
+          ],
+        }),
+      ],
+      width: {
+        size: 100,
+        type: WidthType.PERCENTAGE,
+      },
+      borders: {
+        top: {
+          style: BorderStyle.NONE,
+        },
+        bottom: {
+          style: BorderStyle.NONE,
+        },
+        left: {
+          style: BorderStyle.NONE,
+        },
+        right: {
+          style: BorderStyle.NONE,
+        },
+        insideVertical: {
+          style: BorderStyle.NONE,
+        }
+      },
+    });
   }
 
   public createContactInfo(
